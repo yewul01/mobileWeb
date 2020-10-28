e750f636ec6d0f10a402ac30067d0a547bf3a1ff
yewul01 <jjoham234@daum.net>
2020-10-28 17:59:47 +0900

    보드게임카페 통신중

--- js/myScript.js
+++ js/myScript.js
@@ -1,6 +1,6 @@
 (function ($) {
 
-    // 로고를 클릭하면 main.html의 #content를 load() 하시오.
+    // 로고를 클릭하면 main.html의 #content를 load()
     $('#wrap')
     .on("click", "#header h1 a, #footer .quickMenu a, .mainContent #step_area a, .contTit .prev a",function () {
         var url = this.href;
@@ -10,7 +10,9 @@
       }
     );
 
-    var usedata;
+
+    // 전국 카페
+    var cafe1data;
     $.ajax({
       type:'GET',
       url:'data/cafeStore.json',
@@ -20,13 +22,14 @@
         }
       },
     success : function(data){
-      usedata = data
+      cafe1data = data
     
     },
     error:function(xhr){
       alert(xhr.status + '오류발생')
     }
     })
+    
     
       $('#container').on('click','.storeContent .storeList a', function(e){
         e.preventDefault()
@@ -35,13 +38,54 @@
           $("#container > #content").remove();
           $("#container").load(url + " #content", function(){
             var newContent = '';
-            for (var i in usedata[cafe]) {
-              newContent += `<li><div class="img"><img src="${usedata[cafe][i].photo}" alt=""></div>`
-              newContent += `<div class="storeInfo"><strong>${usedata[cafe][i].name}</strong>`
-              newContent += `<p><i class="far fa-thumbs-up"></i> ${usedata[cafe][i].recommend}</p>`
-              newContent += `<div class="location"><i class="fas fa-map-marker-alt"></i> ${usedata[cafe][i].location}</div></div></li>`
+            for (var i in cafe1data[cafe]) {
+              newContent += `<li><div class="img"><img src="${cafe1data[cafe][i].photo}" alt=""></div>`
+              newContent += `<div class="storeInfo"><strong>${cafe1data[cafe][i].name}</strong>`
+              newContent += `<p><i class="far fa-thumbs-up"></i> ${cafe1data[cafe][i].recommend}</p>`
+              newContent += `<div class="location"><i class="fas fa-map-marker-alt"></i> ${cafe1data[cafe][i].location}</div></div></li>`
             }
             $('#content .part1StoreList').html(`<ul>${newContent}</ul>`)
+          });
+          
+    })
+
+
+
+    // 보드게임 카페
+    var boardData;
+    $.ajax({
+      type:'GET',
+      url:'data/cafeStore.json',
+      beforeSend: function (xhr) {
+        if (xhr.overrideMimeType) {
+          xhr.overrideMimeType("application/json");
+        }
+      },
+    success : function(data){
+      boardData = data
+    
+    },
+    error:function(xhr){
+      alert(xhr.status + '오류발생')
+    }
+    })
+    
+    
+      $('#container').on('click', '.setArea .serch_wrap .board_game a', function(e){
+        e.preventDefault()
+        var url = this.href;
+        var board = $(this).attr('class')
+          $("#container > #content").remove();
+          $("#container").load(url + " #content", function(){
+            var newContent = '';
+            for (var i in boardData[board]) {
+              newContent += `<li><div class="img"><img src="${boardData[board][i].photo}" alt=""></div>`
+              newContent += `<div class="boardGameInfo"><strong>${boardData[board][i].name}</strong>`
+              newContent += `<p class="time"><i class="far fa-clock"></i> ${boardData[board][i].time}</p>`
+              newContent += `<p class="cost"><i class="far fa-won-sign"></i> ${boardData[board][i].cost}<br></p>`
+              newContent += `<div class="location"><i class="fas fa-map-marker-alt"></i> ${boardData[board][i].location}</div></div></li>`
+            }
+            $('#content .boardGameStoreList').html(`<ul>${newContent}</ul>`)
           });
           
     })
