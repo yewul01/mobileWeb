60a13490460610d301f35d4ac2d67fe4b0c71412
yewul01 <jjoham234@daum.net>
2020-11-03 17:36:06 +0900

    햄버거 검색창 버튼 작동오류

--- js/myScript.js
+++ js/myScript.js
@@ -96,12 +96,11 @@
 
     // 햄버거 버튼 클릭하면 네비박스 열기
     $('#lnb_menu').on('click',function(){
-      $(this).next().css({ display:'block' })
-      $('#lnb').animate({left:'0px'}, 500)
+      $(this).next().css({ display:'block' }).animate({left:'0px'}, 500)
     })
     // 햄버거 버튼 클릭하면 네비박스 닫기
     $('#lnb_close').on('click',function(){
-      $('#lnb').animate({ left:'-274px'}, 500, function(){
+      $('#navWrap').animate({ left:'-274px'}, 500, function(){
       $('#navWrap').css({ display:'none'})
       })
     })
